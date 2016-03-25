3aff7481c457239319e5292059071c89eb98d401
Alex Veshch <s.veshcherovych@gmail.com>
2016-03-25 22:57:37 +0200

    add action edit update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -31,6 +31,15 @@ class GroupsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.json { render :show, status: :ok, location: @group }
+      else
+        format.html { render :edit }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
