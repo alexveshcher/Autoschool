565e512112030bcd0267a582a2a01f32da3d1932
Alex Veshch <s.veshcherovych@gmail.com>
2016-03-30 12:45:55 +0300

    add new views to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,14 @@
 class Student < ActiveRecord::Base
 
+  def self.get_all
+    self.find_by_sql('SELECT * FROM students')
+  end
+
+  #returns array of groups, so that used .first
+  def self.get_by_id(id)
+    self.find_by_sql("SELECT * FROM students WHERE id = #{id}").first
+  end
+
+
+
 end
