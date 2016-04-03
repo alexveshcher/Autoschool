d064f0f34682113a3a142bde5e04d5fa5ec3d45a
Alex Veshch <s.veshcherovych@gmail.com>
2016-04-03 18:01:23 +0300

    add Training model and all

---
+++ app/controllers/trainings_controller.rb
@@ -0,0 +1,61 @@
+class TrainingsController < ApplicationController
+  before_action :set_training, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @trainings = Training.all
+  end
+
+  def show
+  end
+
+  def new
+    @training = Training.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @training = Training.new(training_params)
+
+    respond_to do |format|
+      if @training.save
+        format.html { redirect_to @training, notice: 'Training was successfully created.' }
+        #format.json { render :show, status: :created, location: @training }
+      else
+        format.html { render :new }
+        #format.json { render json: @training.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @training.update(training_params)
+        format.html { redirect_to @training, notice: 'Training was successfully updated.' }
+        format.json { render :show, status: :ok, location: @training }
+      else
+        format.html { render :edit }
+        format.json { render json: @training.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @training.destroy
+    respond_to do |format|
+      format.html { redirect_to trainings_url, notice: 'Training was successfully destroyed.' }
+      #format.json { head :no_content }
+    end
+  end
+
+  private
+  def set_training
+    @training = Training.find(params[:id])
+  end
+
+  def training_params
+    params.require(:training).permit(:instructor_id, :vehicle_id, :student_id, :training_time, :duration, :mark, :petrol_usage)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :vehicles
   resources :lections
   resources :instructors
+  resources :trainings
 
   root 'groups#index'
   # The priority is based upon order of creation: first created -> highest priority.
