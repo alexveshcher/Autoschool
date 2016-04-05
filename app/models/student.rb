5a2ccc7fe8424d3fd1061a0a5b2a92eec18cca54
Alex Veshch <s.veshcherovych@gmail.com>
2016-04-05 17:04:47 +0300

    work with bit in student

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@ class Student < ActiveRecord::Base
     self.find_by_sql("SELECT * FROM students WHERE id = #{id}").first
   end
 
+  def passed_school?
+    self.read_attribute(:passed_school) == "\x01" ? true : false
+  end
+
+  def passed_mreo?
+    self.read_attribute(:passed_mreo) == "\x01" ? true : false
+  end
+
 end
