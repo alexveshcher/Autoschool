f5829c88a9c75aa89c496294300d4e8a5bcdda0b
Alex Veshch <s.veshcherovych@gmail.com>
2016-04-05 19:46:03 +0300

    authorization works

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,5 +1,6 @@
 class InstructorsController < ApplicationController
   before_action :set_instructor, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @instructors = Instructor.all
