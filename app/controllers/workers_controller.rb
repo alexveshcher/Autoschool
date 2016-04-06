cd43b467c46d1060ef664847c70cfc39b9210873
Alex Veshch <s.veshcherovych@gmail.com>
2016-04-06 18:57:51 +0300

    make restrictions for student to do unapropriate actions

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,4 +1,5 @@
 class WorkersController < ApplicationController
+  load_and_authorize_resource
   before_action :set_worker, only: [:show, :edit, :update, :destroy]
 
   def index
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,5 +28,11 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+    if user.role == 'student'
+      can :read, ::Worker
+    elsif user.role == 'admin'
+      can :manage, :all
+    end
   end
 end
